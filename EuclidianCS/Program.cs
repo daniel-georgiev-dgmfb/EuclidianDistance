483519b6e4c58841db2b6ac63e5ffd14931c99d0
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-02-04 05:37:39 +0200

    Add project files.

---
+++ EuclidianCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EuclidianCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2, z1, z2;
+            x1 = 12d;
+            x2 = 13d;
+            y1 = 11d;
+            y2 = 10d;
+            z1 = 14d;
+            z2 = 15d;
+            var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            var distance3D = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
+            Console.WriteLine(distance);
+            Console.WriteLine(distance3D);
+        }
+    }
+}
---
+++ EuclidianDistance/EuclidianDistance.py
@@ -0,0 +1,4 @@
+from math import dist
+point_1 = (12,23)
+point_2 = (11,10)
+print(dist(point_1, point_2))
